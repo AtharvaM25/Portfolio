7a3e9a4c0c25df5d49d7fab9a25a0982aa813b30
AtharvaM25 <atharvamahule@gmail.com>
2026-01-10 23:50:57 +0530

    updated the file

--- connect.py
+++ connect.py
@@ -19,4 +19,9 @@ def predict():
 
 
 if __name__ == "__main__":
-    connect.run(debug=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    connect.run(host="0.0.0.0", port=port)
+
