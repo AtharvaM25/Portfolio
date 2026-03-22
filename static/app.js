a30e912e80fadbb10501e3ff2c2e76ea2c8dc6e3
AtharvaM25 <atharvamahule@gmail.com>
2026-03-22 12:59:22 +0530

    connect js

--- static/app.js
+++ static/app.js
@@ -48,8 +48,7 @@ class Chatbot {
         let msg1 = {name: "User", message: text1}
         this.messages.push(msg1);
 
-		//http://127.0.0.1:5000
-        fetch($SCRIPT_ROOT+'/predict', {
+        fetch('/predict', {
             method: 'POST',
             body: JSON.stringify({message: text1}),
             mode: 'cors',
